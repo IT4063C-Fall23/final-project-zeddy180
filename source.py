8e394bea53381dc013e90207dd0d66e0d0b03b74
zsamara <zsams180@gmail.com>
2023-12-03 22:40:35 -0500

    converted the Jupyter Notebook into a Python Script ready for submission

--- source.py
+++ source.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # A Machine Learning Approach to Detecting Cyberbullying in Social Media📝
+# #  Cyberbullying in social media and it's contribution to increased suicide rates among youths📝
 # 
 # ![Banner](./assets/banner.jpeg)
 
@@ -20,6 +20,8 @@
 # -Is there a trend by age on the observed sources and targets of cyberbullying online 
 # 
 # -How can the social media platforms limit or get rid of extreme cyberbullying online 
+# 
+# -Are there any correlations between the countries with most social media users and the number of suicides reported
 
 # ## What would an answer look like?
 # *What is your hypothesized answer to your question?*
@@ -45,9 +47,10 @@
 # 📝 <!-- Start Discussing the project here; you can add as many code cells as you need -->
 # I will use the data from Kaggle to identify the target demographic for the cyberbullying tweets, I then will analyse that data against the suicide data from both Kaggle and cdc to determine the average age group where suicide is more imminent and using the age and gender demographics to find a correlation by linear Regression and visualizations.
 
-# In[3]:
+# In[129]:
 
 
+#importing libraries
 import pandas as pd
 import opendatasets as od
 import numpy as np
@@ -58,6 +61,8 @@ import joblib
 # Python ≥3.10 is required
 import sys
 assert sys.version_info >= (3, 10)
+import statsmodels.api as sm
+
 
 # Scikit Learn imports
 from sklearn.preprocessing import PolynomialFeatures
@@ -83,10 +88,16 @@ np.random.seed(42)
 
 
 
-# In[19]:
+# In[47]:
 
 
-# Start your code here
+from wordcloud import WordCloud 
+
+
+# In[ ]:
+
+
+# Data sources
 
 tweets_url= "https://www.kaggle.com/datasets/soorajtomar/cyberbullying-tweets"
 
@@ -104,19 +115,26 @@ tweets_df.head()
 # In[3]:
 
 
-#suicide_url= "https://www.kaggle.com/datasets/nikhileswarkomati/suicide-watch"
+#Data Source 2
+suicide_url= "https://www.kaggle.com/datasets/nikhileswarkomati/suicide-watch"
 
-#od.download (suicide_url, data_dir= "./data")
+od.download (suicide_url, data_dir= "./data")
 
 
-# In[5]:
+# In[49]:
 
 
 suicide_df = pd.read_csv ('data/suicide.csv')
 
-suicide_df.head()
+suicide_df.head(10)
 
 
+
+
+# In[39]:
+
+
+suicide_df.describe ()
 
 
 # In[8]:
@@ -131,6 +149,12 @@ suicide_df.isnull().sum()
 
 suicide_df.hist (bins=50, figsize= (20, 15) )
 plt.show
+
+
+# In[97]:
+
+
+print(suicide_df.columns.tolist())
 
 
 # In[10]:
@@ -160,6 +184,12 @@ cdc_data_df
 suicide_df.head()
 
 
+# In[79]:
+
+
+suicide_df.info()
+
+
 # In[25]:
 
 
@@ -168,19 +198,88 @@ suicide_df.hist (bins=50, figsize= (20, 15) )
 plt.show
 
 
-# In[24]:
+# In[91]:
 
 
 #Plotting the scatter plot to see how the different variables withing the data relate to each other
-scatter_matrix (suicide_df)
+scatter_matrix (suicide_df[['suicides_no', 'population', 'gdp_per_capita ($)'  ]], figsize=(10, 10))
 plt.show
 
 
-# ## Data Cleaning
-
-# In[27]:
+# In[87]:
 
 
+#Plotting a graph showing how the age groups relate to the number of reported suicide cases
+suicide_df.plot(kind='scatter', x='age', y='suicides_no', alpha=0.1, figsize=(10, 10))
+
+
+# In[37]:
+
+
+#exploring if there exists a correlation between the different columns
+suicide_df.year.corr (suicide_df.suicides_no)
+
+
+# In[55]:
+
+
+#Sorting the dataframe biggest suicide number from the data set
+suicide_df.sort_values (by= "suicides_no", ascending=False).head (10)
+
+
+# In[57]:
+
+
+# Group the data by country and sum the suicides number
+suicides_by_country = suicide_df.groupby('country')['suicides_no'].sum().reset_index()
+
+# Sort the data by suicides number in descending order
+suicides_by_country = suicides_by_country.sort_values('suicides_no', ascending=False)
+suicides_by_country
+
+
+# In[63]:
+
+
+pip install plotly
+
+
+# In[64]:
+
+
+import plotly.express as px
+
+
+# In[81]:
+
+
+# Create the map showing the distibution of the suicide reports across the world
+fig = px.choropleth(suicides_by_country, locations='country', locationmode='country names', color='suicides_no', title='Countries with the Most Suicides Number')
+fig.show()
+
+
+# In[52]:
+
+
+#Generating a wordcloud from the Cyberbullying tweets.
+# Concatenate the text from all rows into a single string
+text = ' '.join(tweets_df['Text'])
+
+# Generate the word cloud
+wordcloud = WordCloud().generate(text)
+
+# Display the word cloud
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+# ## Machine Learning Implementation Process
+
+# In[92]:
+
+
+#Dropping all the missing values from the dataframe
 suicide_without_na = suicide_df.dropna () #removing empty data from the data frame
 
 
@@ -190,32 +289,130 @@ suicide_without_na = suicide_df.dropna () #removing empty data from the data fra
 suicide_without_na.isnull().sum() #Print the new data frame with dropped null values
 
 
-# ## Machine Learning Plan
+# In[114]:
 
-# What type of machine learning model are you planning to use?
-# 
-# I intend on using the regression model in trying to find the best fit line and for the predictive analysis of the discrete data values. For the Cyberbullying tweets, I intend on using the K-Nearest neighbbor algorithm to try and train the model to recognize patterns in words from the word cloud.
-# 
-# What are the challenges have you identified/are you anticipating in building your machine learning model?
-# 
-# The challenges are mostly lack of labeled datasets for most of the sources I find, especially for the cyberbullying datasets. The other challenge is hardship in finding the relation between the datasets due to the missing labels and a clustering attribute.
-# 
-# 
-# How are you planning to address these challenges?
-# 
-# To deal with these challenges, I consider using active-learning, which may include selecting the most useful information from the unlabelled datasets and labelling them for an easy reference and training of the model. I also consider restructuring the datasets to only extract the columns that may be the most useful for this project.
 
-# ## Machine Learning Implementation Process
+#Splitting the data set
 
-# - Extract the age column from the suicide data
-# -Extract the number of suicide reports from the data
-# -Remove all the empty values from the data
-# -Find the Mean
-# -Impute the empty values with the mean
-# -Plot a graph of the age against the number of suicide reported and label the age groups\
-# -Compute the RMSE for the number of suicide reported
-# -Separate the data into Train and Test data and display the best fit line.
-# -Use the pipeline to predict the future trends using the best fit line.
+train_set, test_set = train_test_split (suicide_df, test_size=0.2, random_state=45)
+train_set.head(10)
+
+
+# In[115]:
+
+
+#separating the predictor from the target
+suicide_X = train_set.drop ('suicides_no', axis=1)
+suicide_Y = train_set['suicides_no'].copy()
+
+
+# In[116]:
+
+
+#Separating the numerical data from the categorical data and imputing the dataframe columns
+from sklearn.impute import SimpleImputer
+
+imputer= SimpleImputer (strategy='median')
+
+suicide_num = suicide_X.drop(['country', 'sex', 'age', 'country-year', ' gdp_for_year ($) ', 'generation'],axis=1)
+
+imputer.fit(suicide_num)
+
+
+# In[117]:
+
+
+imputer.statistics_
+
+
+# In[118]:
+
+
+#Viewing the correlation matrix for the numerical data
+suicide_num.corr()
+
+
+# In[119]:
+
+
+#Handling Categorical Data
+suicide_cat = suicide_X[['country', 'sex', 'age', 'country-year', ' gdp_for_year ($) ', 'generation']] 
+
+
+# In[120]:
+
+
+#Transforming the Categorical Data
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder= OrdinalEncoder()
+suicide_cat_encoded = ordinal_encoder.fit_transform (suicide_cat)
+
+
+
+# In[121]:
+
+
+suicide_cat_encoded
+
+
+# In[122]:
+
+
+#Using OneHotEncoder to encode the categorical data
+from sklearn.preprocessing import OneHotEncoder
+oneHot_encoder= OneHotEncoder()
+suicide_cat_hot = oneHot_encoder.fit_transform(suicide_cat)
+
+suicide_cat_hot.toarray()
+
+
+# In[123]:
+
+
+#Using the Pipeline to fit our numerical and categorical transformed data
+from sklearn.pipeline import Pipeline
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median'))
+])
+suicide_num_tr = num_pipeline.fit_transform(suicide_num)
+
+
+# In[124]:
+
+
+cat_pipeline = Pipeline([
+    ('one-hot-encode', OneHotEncoder())
+])
+suicide_cat_tr = cat_pipeline.fit_transform(suicide_cat)
+
+
+# In[127]:
+
+
+#Performing the full Pipeline fitting
+from sklearn.compose import ColumnTransformer
+
+num_features = suicide_num.columns
+cat_features = suicide_cat.columns
+
+full_pipeline = ColumnTransformer ([
+    ('num', num_pipeline, num_features),
+    ('cat', cat_pipeline, cat_features)
+])
+clean_suicide = full_pipeline.fit_transform (suicide_X)
+
+
+# In[131]:
+
+
+#Linear Regression Analysis
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression
+lin_reg.fit (clean_suicide, suicide_Y)
+
 
 # ## Peer feedback
 # 
@@ -233,11 +430,17 @@ suicide_without_na.isnull().sum() #Print the new data frame with dropped null va
 # -https://medium.com/linkit-intecs/how-to-upload-large-files-to-github-repository-2b1e03723d2
 # 
 # -https://www.pewresearch.org/search/cyberbullying
+# 
+# -stackoverflow.com
+# 
+# -W3schools.com
+# 
+# -Youtube.com
 
 # # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
 # !jupyter nbconvert --to python source.ipynb
 
-# In[26]:
+# In[30]:
 
 
 get_ipython().system('jupyter nbconvert --to python source.ipynb')
